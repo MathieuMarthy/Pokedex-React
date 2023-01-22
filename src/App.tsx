2c6e2a24375da2845773fc596f4c7cf0a4533d9d
Mathieu Marthy <l4p0ubelle@gmail.com>
2023-01-22 12:28:09 +0100

    pokemons|mock-pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-  
-const App: React.FC = () => {
- const name: String = "React";
-    
- return (
-  <h1>Hello, {name} !</h1>
- )
+import React, {FunctionComponent, useState} from "react";
+import POKEMONS from "./models/mock-pokemon"
+import Pokemon from "./models/pokemon";
+
+
+const App: FunctionComponent = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>(POKEMONS);
+
+    return (
+        <h1>Il y a {pokemons.length} de pokemons dans le Pokédex</h1>
+    )
 }
-  
+
 export default App;
